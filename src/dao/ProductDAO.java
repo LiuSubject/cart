4445c14152d81fd879e470586a833a9eef913807
liuxueke <xueke.liu@outlook.com>
2017-09-14 21:35:18 +0800

    2017-9-14 21:35:15

--- src/dao/ProductDAO.java
+++ src/dao/ProductDAO.java
@@ -14,8 +14,50 @@ public class ProductDAO {
  
     public static void main(String[] args) {
  
-        System.out.println(new ProductDAO().ListProduct().size());
+        System.out.println(new ProductDAO().getProduct(1).getName());
  
+    }
+ 
+    public Product getProduct(int id) {
+        Product result = null;
+ 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+ 
+            Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cart?characterEncoding=UTF-8",
+                    "root", "admin");
+ 
+            String sql = "select * from product where id = ?";
+ 
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setInt(1, id);
+ 
+            ResultSet rs = ps.executeQuery();
+ 
+            if (rs.next()) {
+                result = new Product();
+                result.setId(id);
+ 
+                String name = rs.getString(2);
+                float price = rs.getFloat(3);
+ 
+                result.setName(name);
+                result.setPrice(price);
+ 
+            }
+ 
+            ps.close();
+ 
+            c.close();
+ 
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return result;
     }
  
     public List<Product> ListProduct() {
---
+++ src/servlet/OrderItemDeleteServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+ 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import bean.OrderItem;
+import bean.Product;
+import dao.ProductDAO;
+ 
+public class OrderItemDeleteServlet extends HttpServlet {
+ 
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+        int pid = Integer.parseInt(request.getParameter("pid"));
+ 
+        List<OrderItem> ois = (List<OrderItem>) request.getSession().getAttribute("ois");
+ 
+        /*if (null != ois) {
+        	for (OrderItem orderItem : ois) {
+                if (orderItem.getProduct().getId() == pid) {
+                	ois.remove(orderItem);
+                }
+            }
+        }*/
+        List<OrderItem> ois4Delete =new ArrayList<>();
+        if(null!=ois){
+        	for (OrderItem io : ois) {
+				if(io.getProduct().getId()==pid){
+					ois4Delete.add(io);
+				}
+			}
+        }
+        ois.removeAll(ois4Delete);
+ 
+        response.sendRedirect("/cart/listOrderItem");
+ 
+    }
+}
